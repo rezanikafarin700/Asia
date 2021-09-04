e0c95332ab3f4498709847150c574b66d9ebeacc
rezanikafarin <rezanikafarin100@gmail.com>
2021-09-04 21:05:28 +0430

    create ProductRequest

---
+++ app/Http/Requests/Product/ProductRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests\Product;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProductRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|min:2|max:50',
+            'model' => 'required|min:2|max:50',
+            'material' => 'required|min:2|max:50',
+            'size' => 'required|min:2|max:50',
+            'code' => 'required|min:1|max:3',
+            'price' => 'required|min:1|max:10',
+            'discount' => 'nullable',
+            'description' => 'nullable',
+            'images' => 'nullable|array|max:10',
+            'images.*' => 'image|max:2000'
+
+        ];
+    }
+}
