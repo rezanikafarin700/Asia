0977e93264fb826f9cd951651f3acf3b2291fe78
rezanikafarin <rezanikafarin100@gmail.com>
2021-09-04 20:22:15 +0430

    create UserRequest

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\User\UserRequest;
 use App\User;
 use Illuminate\Foundation\Auth\Verifies;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
-    public function store(Request $request)
+    public function store(UserRequest $request)
     {
         $user =  User::create([
             'email' => $request->email,
